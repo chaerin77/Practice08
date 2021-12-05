e5832895ebff70d7ac7ef6c22c48237e93df5741
chaerin <77728068+Boram8878@users.noreply.github.com>
2021-12-05 23:23:22 +0900

    practice08 -ex08

--- src/com/javaex/ex08/Friend.java
+++ src/com/javaex/ex08/Friend.java
@@ -2,4 +2,52 @@ package com.javaex.ex08;
 
 public class Friend {
 
+	//필드
+	private String name;
+	private String ph;
+	private String school;
+	
+	//생성자
+	public Friend() {
+		
+	}
+	
+	//메소드g/s
+
+	public Friend(String name, String ph, String school) {
+		this.name=name;
+		this.ph=ph;
+		this.school=school;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPh() {
+		return ph;
+	}
+
+	public void setPh(String ph) {
+		this.ph = ph;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	
+	
+	//메소드 일반
+	public void showInfo() {
+		System.out.println("이름:" + name + " 핸드폰:" + ph + " 학교:" + school);
+	}
+	
 }
--- src/com/javaex/ex08/FriendApp.java
+++ src/com/javaex/ex08/FriendApp.java
@@ -1,9 +1,32 @@
 package com.javaex.ex08;
 
+import java.util.Scanner;
+
 public class FriendApp {
 
 	public static void main(String[] args) {
 
+		System.out.println("친구를 3명 등록해 주세요");
+		Scanner sc = new Scanner(System.in);
+		
+		String Woo = sc.nextLine();
+		String[] wArray = Woo.split(" ");
+		
+		String Lee = sc.nextLine();
+		String[] iArray = Lee.split(" ");
+		
+		String Jae = sc.nextLine();
+		String[] jArray = Jae.split(" ");
+		
+		Friend W = new Friend(wArray[0],wArray[1],wArray[2]);
+		
+	    Friend L = new Friend(iArray[0],iArray[1],iArray[2]);
+	
+	    Friend J = new Friend(jArray[0],jArray[1],jArray[2]);
+	
+	    W.showInfo();
+	    L.showInfo();
+	    J.showInfo();
 	}
 
 }
